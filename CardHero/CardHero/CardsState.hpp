896ac4ee91294746bd2129e6e149a50163ec78ba
mgracanin <mijo.gracanin@gmail.com>
2018-11-12 16:37:20 +0100

    Add CardsState

---
+++ CardHero/CardHero/CardsState.hpp
@@ -0,0 +1,34 @@
+//
+//  CardsState.hpp
+//  CardHero
+//
+//  Created by Mijo Gracanin on 04/11/2018.
+//  Copyright © 2018 MijoCoder. All rights reserved.
+//
+
+#ifndef CardsState_hpp
+#define CardsState_hpp
+
+#include <SFML/Graphics.hpp>
+#include "State.h"
+#include "Game.h"
+
+namespace as
+{
+    class CardsState : public State
+    {
+    public:
+        CardsState(GameDataRef data);
+        
+        void Init();
+        
+        void HandleInput();
+        void Update(float dt);
+        void Draw(float dt);
+        
+    private:
+        GameDataRef _data;
+    };
+}
+
+#endif /* CardsState_hpp */
